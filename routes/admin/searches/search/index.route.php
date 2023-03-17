eeb8fd05c7fc2e1aaa08a3e9305fa6122850b8df
kiptoo <felixkpt@gmail.com>
2023-03-17 23:20:06 +0300

    autogenerate list updated 2

--- routes/admin/searches/search/index.route.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\Searches\Search\SearchController;
-
-Route::get('/{id}', [SearchController::class, 'index']);
-Route::post('/{id}', [SearchController::class, 'storeSearch']);
-Route::post('/toggle-status/{id}', [SearchController::class, 'toggleSearchStatus']);
-Route::delete('/delete/{id}', [SearchController::class, 'destroySearch']);
