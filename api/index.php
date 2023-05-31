5c4042dcc5fba63f7cc7cb7202bd83c717cdeb77
kiptoo <felixkpt@gmail.com>
2023-05-31 19:45:59 +0300

    api/index.php fix

--- api/index.php
+++ api/index.php
@@ -5,12 +5,15 @@
  * Manually route assets to be found
  * https://github.com/juicyfx/vercel-examples/commit/1fcbe3ff98ae34830cfd779224433cca16bb4f93
  */
-if ($_GET['type'] === 'css') {
-    header("Content-type: text/css; charset: UTF-8");
-    echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
-} else if ($_GET['type'] === 'js') {
-    header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+if (isset($_GET['type']) && isset($_GET['file'])) {
+
+    if ($_GET['type'] === 'css') {
+        header("Content-type: text/css; charset: UTF-8");
+        echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
+    } else if ($_GET['type'] === 'js') {
+        header('Content-Type: application/javascript; charset: UTF-8');
+        echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+    }
 } else {
     // Forward Vercel requests to normal index.php
     require __DIR__ . '/../public/index.php';
